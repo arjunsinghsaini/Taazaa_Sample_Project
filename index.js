7102d3a1da8e8d0a0d1ab6da9bf7e13c6c0201e7
Arjun <arjun.singh@affle.com>
2021-02-13 12:40:46 +0530

    network layer added and api get hit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+import HomePage from './src/screens/HomePage'
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(HomePage);
